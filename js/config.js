e781ae563e5b073f878321627d7151223675ad6e
Lucas Boavida <161470866+LucasBoavida@users.noreply.github.com>
2026-09-04 13:59:54 +0900

    config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,18 @@
+// Konfigurasaun Firebase Database
+export const firebaseConfig = {
+    apiKey: "AIzaSySyFktnxXLR0P3EnBMf7p1DEwdShB8",
+    authDomain: "fish-feeder-db.firebaseapp.com",
+    databaseURL: "https://fish-feeder-db-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "fish-feeder-db",
+    storageBucket: "fish-feeder-db.firebasestorage.app",
+    messagingSenderId: "498255146529",
+    appId: "1:498255146529:web:cabcdaf269e970c941bd60"
+};
 
+// Konfigurasaun Default HiveMQ MQTT Broker
+export const mqttConfig = {
+    host: "616936635376438c8d09444543e28601.s1.eu.hivemq.cloud",
+    port: 8884,
+    username: "fishfeeder",
+    password: "fishfeeder26"
+};
