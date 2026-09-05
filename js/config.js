8099d81f6bd2a3f08c944ecd7884831b0cc698d4
Lucas Boavida <161470866+LucasBoavida@users.noreply.github.com>
2026-09-05 14:37:46 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 // Konfigurasaun Firebase Database
 export const firebaseConfig = {
-    apiKey: "AIzaSySyFktnxXLR0P3EnBMf7p1DEwdShB8",
-    authDomain: "fish-feeder-db.firebaseapp.com",
-    databaseURL: "https://fish-feeder-db-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "fish-feeder-db",
-    storageBucket: "fish-feeder-db.firebasestorage.app",
-    messagingSenderId: "498255146529",
-    appId: "1:498255146529:web:cabcdaf269e970c941bd60"
+    apiKey: "AIzaSySySyFktnxXlR0P3EnBMf7p1DEwdSHb8",
+      authDomain: "fish-feeder-db.firebaseapp.com",
+      database URL: "https://fish-feeder-db-default-rtdb.asia-southeast1.firebasedatabase.app",
+      projectId: "fish-feeder-db",
+      storageBucket: "fish-feeder-db.firebasestorage.app",
+      messagingSenderId: "498255146529",
+      appId: "1:498255146529:web:1cfc310a6461e348441bd60",
+      measurementId: "G-32L08HNN52"
 };
 
 // Konfigurasaun Default HiveMQ MQTT Broker
